e7863a22e65e66f961d7305cad4f5171d9764b0a
Redzi30 <redzephabibovic30@gmail.com>
2023-05-22 11:20:17 +0200

    update novi

--- Vezbe 16/app.js
+++ Vezbe 16/app.js
@@ -641,108 +641,41 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-   
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let a = 5;
+function prva(){
+    console.log(a);
+}
+function druga(){
+    let a = 3;
+    console.log(a);
+}
+function treca(){
+    let a = 4;
+    console.log(a);
+}
+
+prva();
+druga();
+treca();
+
+
+
+
+
+class Car {
+    marka;
+    model;
+    motor = "2.0";
+    menjac;
+    constructor(markaauta, modelauta,  menjacauta){
+        this.marka = markaauta;
+        this.model = modelauta;
+        this.menjac = menjacauta;
+    }
+}
+
+const podaciAuta = new Car("Audi","A4", "Saltac");
+
+console.log(podaciAuta);
 
 
